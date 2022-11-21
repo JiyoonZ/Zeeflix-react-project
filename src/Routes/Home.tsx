0ce8bc19655f3e151589b1dea7d02f683c58aa28
jiyoonZ <wldbsl416@gmail.com>
2022-11-22 00:48:32 +0900

    Feat : slider 를 pagination 처럼

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -4,6 +4,7 @@ import {getMovies, IGetMoviesResult} from "../api";
 import {makeImagePath} from "../utils";
 import {motion, AnimatePresence} from "framer-motion";
 import {useState} from "react";
+import {resourceLimits} from "worker_threads";
 
 const Wrapper = styled.div`
   background-color: black;
@@ -40,40 +41,50 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
   display: grid;
-  gap: 10px;
+  gap: 5px;
   grid-template-columns: repeat(6, 1fr);
   position: absolute;
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{bgPath: string}>`
   background-color: white;
   height: 200px;
-  color: red;
   font-size: 40px;
+  background-image: url(${(props) => props.bgPath});
+  background-size: cover;
+  background-position: center center;
 `;
+
 const rowVars = {
   hidden: {
-    x: window.outerWidth + 10,
+    x: window.outerWidth + 5,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.outerWidth - 10,
+    x: -window.outerWidth - 5,
   },
 };
-
+const offset = 6; //한슬라이드에 보여줄 영와의 갯수
 function Home() {
   const {data, isLoading} = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0); // 페이지 번호 , index
   const increaseIndex = () => {
-    if (leaving) return;
-    setLeaving(true);
-    setIndex((prev) => prev + 1);
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      // 총 영화의 갯수 (배너에있느 영화는 제외)
+      const totalMovies = data.results.length - 1;
+      // 최대 페이지 , 19개라면 19개/6개씩 3.13 따라서 총 4페이지로 반올림후 index는 zerobase 이므로 -1
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
   };
   const [leaving, setLeaving] = useState(false);
   const toggleLeaving = () => setLeaving((prev) => !prev);
@@ -86,10 +97,10 @@ function Home() {
         <>
           <Banner
             onClick={increaseIndex}
-            bgPath={makeImagePath(data?.results[2].backdrop_path || "")}
+            bgPath={makeImagePath(data?.results[0].backdrop_path || "")}
           >
-            <Title>{data?.results[2].title}</Title>
-            <Overview>{data?.results[2].overview}</Overview>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
             <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
@@ -101,9 +112,17 @@ function Home() {
                 transition={{type: "tween", duration: 1}}
                 key={index}
               >
-                {[1, 2, 3, 4, 5, 6].map((i) => (
-                  <Box key={i}>{i}</Box>
-                ))}
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bgPath={makeImagePath(movie.backdrop_path, "w500")}
+                    >
+                      {movie.title}
+                    </Box>
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
