db2a8d32ac3a9146afb4ce64d036b6425f0c4c45
jiyoonZ <wldbsl416@gmail.com>
2022-11-24 15:51:58 +0900

    Feat : tvshow api

--- src/api.ts
+++ src/api.ts
@@ -19,14 +19,27 @@ export interface IGetMoviesResult {
   total_pages: number;
   total_results: number;
 }
+
 export enum CategoryType {
   "now_playing" = "now_playing",
+  "upcoming" = "upcoming",
   "popular" = "popular",
   "top_rated" = "top_rated",
-  "upcoming" = "upcoming",
 }
 export async function getMovies(category: CategoryType) {
   return await fetch(
     `${BASE_PATH}/movie/${category}?api_key=${API_KEY}&page=1&region=kr`
+  ).then((resp) => resp.json());
+}
+
+export async function getTvShow(tvCategory: CategoryType) {
+  const categoryValue =
+    tvCategory === "now_playing"
+      ? "on_the_air"
+      : tvCategory === "upcoming"
+      ? "airing_today"
+      : tvCategory;
+  return await fetch(
+    `${BASE_PATH}/tv/${categoryValue}?api_key=${API_KEY}&language=en-US&page=1`
   ).then((resp) => resp.json());
 }
