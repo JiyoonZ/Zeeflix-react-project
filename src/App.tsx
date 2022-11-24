3acf25a10e4a90b98d274abe07cd966d43e31863
jiyoonZ <wldbsl416@gmail.com>
2022-11-24 16:41:37 +0900

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
